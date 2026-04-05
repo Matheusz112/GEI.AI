6ea3989799c2879ce52363a1d66681a9187cdb8f
CORDTECMATH <127919734+Matheusz112@users.noreply.github.com>
2026-04-05 16:41:18 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Suporte para arquivos .cjs (se necessário)
 config.resolver.assetExts.push('cjs');
 
 module.exports = config;
